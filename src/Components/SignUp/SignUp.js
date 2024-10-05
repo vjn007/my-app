560d694fe39732fd2b1ce117d7660b2c431c59b5
S VIJAYAN <seelavijayan@gmail.com>
2024-10-05 12:31:36 +0530

    main code

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -3,221 +3,283 @@ import "../../Assets/Css/SignUp/signUp.css";
 import { useNavigation } from "../../Utils/Functions/Navigation/Navigation";
 
 const SignUp = () => {
-    // Single state object to handle all form inputs
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        middleName: '',
-        phoneNumber: '',
-        dateOfBirth: '',
-        gender: '',
-        image: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    middleName: "",
+    phoneNumber: "",
+    dateOfBirth: "",
+    gender: "",
+    image: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  let navigateTo = useNavigation();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
+  };
 
-    const [errors, setErrors] = useState({});
-    let navigateTo = useNavigation();
+  const validateForm = () => {
+    const newErrors = {};
 
-    // Handle input change for all fields
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+    if (!formData.firstName) {
+      newErrors.firstName = "First Name is required";
+    }
 
-    // Validation logic
-    const validateForm = () => {
-        const newErrors = {};
+    if (!formData.lastName) {
+      newErrors.lastName = "Last Name is required";
+    }
 
-        // First Name validation
-        if (!formData.firstName) {
-            newErrors.firstName = 'First Name is required';
-        }
+    if (!formData.phoneNumber) {
+      newErrors.phoneNumber = "Phone Number is required";
+    } else if (!/^[6-9]\d{9}$/.test(formData.phoneNumber)) {
+      newErrors.phoneNumber = "Phone Number is invalid";
+    }
 
-        // Last Name validation
-        if (!formData.lastName) {
-            newErrors.lastName = 'Last Name is required';
-        }
+    if (!formData.dateOfBirth) {
+      newErrors.dateOfBirth = "Date Of Birth is required";
+    }
 
-        // Phone Number Validation
-        if (!formData.phoneNumber) {
-            newErrors.phoneNumber = 'Phone Number is required';
-        }
+    if (!formData.gender) {
+      newErrors.gender = "Please select Gender";
+    }
 
-        //Date Of Birth validation
-        if (!formData.dateOfBirth) {
-            newErrors.dateOfBirth = 'Date Of Birth is required';
-        }
+    if (!formData.image) {
+      newErrors.image = "Image is required";
+    }
 
-        // Gender validation
-        if (!formData.gender) {
-            newErrors.gender = 'Please Select Gender';
-        }
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Email is invalid";
+    }
 
-        //image validation
-        if (!formData.image) {
-            newErrors.image = 'Image is required';
-        }
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters";
+    }
 
-        // Email validation
-        if (!formData.email) {
-            newErrors.email = 'Email is required';
-        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-            newErrors.email = 'Email is invalid';
-        }
+    if (formData.confirmPassword !== formData.password) {
+      newErrors.confirmPassword = "Passwords do not match";
+    } else if (!formData.confirmPassword) {
+      newErrors.confirmPassword = "Confirm Password is required";
+    }
 
-        // Password validation
-        if (!formData.password) {
-            newErrors.password = 'Password is required';
-        } else if (formData.password.length < 6) {
-            newErrors.password = 'Password must be at least 6 characters';
-        }
+    return newErrors;
+  };
 
-        // Confirm Password validation
-        if (formData.confirmPassword !== formData.password) {
-            newErrors.confirmPassword = 'Passwords do not match';
-        } else if (!formData.confirmPassword) {
-            newErrors.confirmPassword = 'Confirm Password is required';
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({});
+      navigateTo("/sign-in");
+      console.log("Sign Up Successful:", formData);
+    }
+  };
 
-        return newErrors;
-    };
+  return (
+    <div className="signUp">
+      <div className="form-container">
+        <div className="form-box">
+          <h1>Sign Up</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>First Name</label>
+                <span className="required">*</span>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                {errors.firstName && (
+                  <p className="error">{errors.firstName}</p>
+                )}
+              </div>
 
-    // Handle form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const validationErrors = validateForm();
-        if (Object.keys(validationErrors).length > 0) {
-            setErrors(validationErrors);
-        } else {
-            console.log('Sign Up Successful:', formData);
-            // Add your sign-up logic here
-        }
-    };
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Last Name</label>
+                <span className="required">*</span>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                {errors.lastName && <p className="error">{errors.lastName}</p>}
+              </div>
 
-    return (
-        <div className="signUp">
-            <div className="form-container">
-                <div className="form-box">
-                    <h1>Sign Up</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div className="row">
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>First Name</label>
-                                <input
-                                    type="text"
-                                    name="firstName"
-                                    value={formData.firstName}
-                                    onChange={handleChange}
-                                />
-                                {errors.firstName && <p className="error">{errors.firstName}</p>}
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Last Name</label>
-                                <input
-                                    type="text"
-                                    name="lastName"
-                                    value={formData.lastName}
-                                    onChange={handleChange}
-                                />
-                                {errors.lastName && <p className="error">{errors.lastName}</p>}
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Middle Name</label>
-                                <input
-                                    type="text"
-                                    name="middleName"
-                                    value={formData.middleName}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Date Of Birth</label>
-                                <input
-                                    type="date"
-                                    name="dateOfBirth"
-                                    value={formData.dateOfBirth}
-                                    onChange={handleChange}
-                                />
-                                {errors.dateOfBirth && <p className="error">{errors.dateOfBirth}</p>}
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Phone Number</label>
-                                <input
-                                    type="number"
-                                    name="phoneNumber"
-                                    value={formData.phoneNumber}
-                                    onChange={handleChange}
-                                />
-                                {errors.phoneNumber && <p className="error">{errors.phoneNumber}</p>}
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Email</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                />
-                                {errors.email && <p className="error">{errors.email}</p>}
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Gender</label>
-                                <input
-                                    type="text"
-                                    name="gender"
-                                    value={formData.gender}
-                                    onChange={handleChange}
-                                />
-                                {errors.gender && <p className="error">{errors.gender}</p>}
-                            </div>
-                            <div className="col-lg-8 col-md-10 col-sm-12 col-xs-12">
-                                <label>Image</label>
-                                <input
-                                    type="file"
-                                    name="image"
-                                    value={formData.image}
-                                    onChange={handleChange}
-                                />
-                                {errors.image && <p className="error">{errors.image}</p>}
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                />
-                                {errors.password && <p className="error">{errors.password}</p>}
-                            </div>
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                                <label>Confirm Password</label>
-                                <input
-                                    type="password"
-                                    name="confirmPassword"
-                                    value={formData.confirmPassword}
-                                    onChange={handleChange}
-                                />
-                                {errors.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
-                            </div>
-                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <button type="submit" className="submit-btn">Sign Up</button>
-                            </div>
-                        </div>
-                    </form>
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Middle Name</label>
+                <input
+                  type="text"
+                  name="middleName"
+                  value={formData.middleName}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+              </div>
 
-                    <div className="mt-4">
-                        Click Here To <span className="cursor" onClick={() => navigateTo("/sign-in")}>Sign In</span>
-                    </div>
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Date Of Birth</label>
+                <span className="required">*</span>
+                <input
+                  type="date"
+                  name="dateOfBirth"
+                  value={formData.dateOfBirth}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                {errors.dateOfBirth && (
+                  <p className="error">{errors.dateOfBirth}</p>
+                )}
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Phone Number</label>
+                <span className="required">*</span>
+                <input
+                  type="number"
+                  name="phoneNumber"
+                  value={formData.phoneNumber}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                {errors.phoneNumber && (
+                  <p className="error">{errors.phoneNumber}</p>
+                )}
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Email</label>
+                <span className="required">*</span>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                {errors.email && <p className="error">{errors.email}</p>}
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Gender</label>
+                <span className="required">*</span>
+                <div className="d-flex justify-content-around align-items-center">
+                  <label>
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Male"
+                      checked={formData.gender === "Male"}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  Male
+                  <label>
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Female"
+                      checked={formData.gender === "Female"}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  Female
+                  <label>
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Other"
+                      checked={formData.gender === "Other"}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  Other
                 </div>
+                {errors.gender && <p className="error mt-2">{errors.gender}</p>}
+              </div>
+
+              <div className="col-lg-8 col-md-10 col-sm-12 col-xs-12">
+                <label>Image</label>
+                <span className="required">*</span>
+                <input
+                  type="file"
+                  name="image"
+                  onChange={(e) =>
+                    setFormData({ ...formData, image: e.target.files[0] })
+                  }
+                />
+                {errors.image && <p className="error">{errors.image}</p>}
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Password</label>
+                <span className="required">*</span>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                {errors.password && <p className="error">{errors.password}</p>}
+              </div>
+
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <label>Confirm Password</label>
+                <span className="required">*</span>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                {errors.confirmPassword && (
+                  <p className="error">{errors.confirmPassword}</p>
+                )}
+              </div>
             </div>
+          </form>
+
+          <div className="mt-4 d-flex justify-content-between align-items-center">
+            <div>
+              Click Here To{" "}
+              <span className="cursor" onClick={() => navigateTo("/sign-in")}>
+                Sign In
+              </span>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="submit-btn"
+                onClick={handleSubmit}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
